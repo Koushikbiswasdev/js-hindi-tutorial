efacc6bf4fcb07e75cfa67214ce355d5cf0432b0
koushikbiswasdev <51769408+Koushikbiswasdev@users.noreply.github.com>
2024-08-04 02:00:20 +0000

    datatype

---
+++ 01_Basic/02_datatype.js
@@ -0,0 +1,30 @@
+"use strict"; // Treat all js code as newer version
+// alert (3+3); // We are using nodejs not browser
+
+console.log(3
+    +3); // code readability should be high
+console.log("koushik");
+
+let name = "koushik" // string datatype
+let age = 42 // number datatype
+let  isLoggedIn = true // boolean datatype
+let state = null
+
+// number =>
+// bigint =>
+// string => ""
+// boolean => true/false
+// null (standalon value)
+// undefined (value not assigned)
+// symbol => Unique
+
+
+// object
+
+console.log(typeof "koushik");
+console.log(typeof age);
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
+
+
+
